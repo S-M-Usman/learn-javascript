26a6d862e6f8ba3dac3f322024c2645ecca5dd5b
Shaikh Muhammad Usman <shaikhmuhammadusmanofficial@gmail.com>
2024-09-11 05:00:22 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,31 @@
+let score = "hitesh"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+/*
+ 33 => number
+ "33abc"=> NaN (Not a number)
+ true => 1 false => 0
+*/
+let isLoggedin = ""
+let booleanIsLoggedIn = Boolean(isLoggedin)
+console.log(booleanIsLoggedIn);
+/*
+ 1 =>true
+ 2 =>false
+ 
+ 1 =>true
+ 2 =>false
+ "" =>false
+ "h" =>true
+
+ */
+let someNumber =99
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
