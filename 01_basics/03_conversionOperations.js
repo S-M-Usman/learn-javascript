587280ad8b4a266841ccb405e33069015036ed70
Shaikh Muhammad Usman <shaikhmuhammadusmanofficial@gmail.com>
2024-09-11 05:30:14 +0000

    conversions and Operation

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -13,7 +13,7 @@ let valueInNumber = Number(score);
 */
 let isLoggedin = ""
 let booleanIsLoggedIn = Boolean(isLoggedin)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 /*
  1 =>true
  2 =>false
@@ -27,5 +27,47 @@ console.log(booleanIsLoggedIn);
 let someNumber =99
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//*************** OPERATIONS ******************* 
+let value = 3
+let negValue = -value;
+// console.log(negValue);
+
+
+// console.log(3+3);
+// console.log(3*3);
+// console.log(3-3);
+// console.log(3/3);
+// console.log(3**3);
+// console.log(3***3);
+// console.log(3%3);
+
+let str1 = 'hello'
+let str2 = ' hitesh'
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log((3+4)*3%4);
+
+// console.log(true);
+// console.log(+true );
+// console.log(+"");
+
+let num1,num2,num3
+num1 = num2=num3 = 2+2
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+
+
+
+
+
