87c39a5c5092c4571d632900d3c9971057c80d15
Shaikh Muhammad Usman <shaikhmuhammadusmanofficial@gmail.com>
2024-09-11 04:44:24 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict";//treat all js as newer version
+// alert('Hello') // we are using node.js not browser
+console.log(3
+    +3)//code readability should be high
+console.log('hitesh');
+
+let name = 'usman';
+let age = 19;
+let isLoggedin = false;
+
+//number - range (from  0  to 2 to power53)
+//string => '',"".``
+//boolean => true/false
+//null=>standalone value(representation of any value)
+//undefined =>(unintentonal)
+//symbol => for uniqueness
+//objects
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof null)//object
+console.log(typeof undefined)//null
